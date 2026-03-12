e8177466f6f87aab4ceb175eb8b5251bd1caad8a
Devin Halser <devin.hasler@bluewin.ch>
2026-03-12 19:29:49 +0100

    added migrate step

--- apps/backend/db/index.ts
+++ apps/backend/db/index.ts
@@ -2,8 +2,11 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema.js";
+import { migrate } from "drizzle-orm/node-postgres/migrator";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 export const db = drizzle({ client: pool, schema });
+
+await migrate(db, { migrationsFolder: "./db/drizzle" });
